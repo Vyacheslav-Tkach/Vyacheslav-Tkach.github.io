cf13618f2f4c3753985718922f2105eda2c89178
Vyacheslav-Tkach <44356277+Vyacheslav-Tkach@users.noreply.github.com>
2018-11-27 17:21:58 +0300

    Add files via upload

---
+++ js/main_js.js
@@ -0,0 +1,23 @@
+
+$(function(){ 
+$(window).scroll(function(){ 
+if($(this).scrollTop()!=0){ 
+$('.btn-up').fadeIn(); 
+} 
+else{ 
+$('.btn-up').fadeOut(); 
+} 
+}); 
+$('.btn-up').click(function(){ 
+$('body,html').animate({scrollTop:0},500); 
+}); 
+}); 
+$(function() { 
+$('a[href="#scroll"]').click(function(event) { 
+event.preventDefault() 
+var el = $('.block-scroll'); 
+$('body,html').animate({ 
+scrollTop: $(el).offset().top 
+}, 1000); 
+}); 
+}); 
